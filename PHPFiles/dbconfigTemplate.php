a0b8aa01d47c7dd18172b954c34e6948dba77d1f
wayko621 <wayko@users.noreply.github.com>
2025-02-25 11:43:24 -0500

    Update dbconfigTemplage.php
    Update dbconfigTemplage.php

--- PHPFiles/dbconfigTemplate.php
+++ PHPFiles/dbconfigTemplate.php
@@ -1,5 +1,5 @@
 <?php
-$con = new mysqli('localhost', 'username', 'password', 'databasename');
+$con = new mysqli('localhost', 'cpanelusername_databaseusername', 'password', 'cpanelusername_databasename');
 if ($con->connect_error) {
   die("Connection failed: " . $con->connect_error);
 }
