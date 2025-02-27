f80c18ce2fe086e1bdede72258eef37f113047d7
wayko621 <wayko@users.noreply.github.com>
2025-02-27 10:06:01 -0500

    added verification for adding users
    added verification for adding users. If the email exist the account will not be created

--- PHPFiles/addAdminUser.php
+++ PHPFiles/addAdminUser.php
@@ -1,31 +1,42 @@
 <?php
-$adminID = $_POST['adminID'];
-$hashPassword = hash('sha256', $adminID);
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$email = $_POST['email'];
-$tlevel = $_POST['tlevel'];
+   $adminID = $_POST['adminID'];
+   $hashPassword = hash('sha256', $adminID);
+   $fname = $_POST['fname'];
+   $lname = $_POST['lname'];
+   $email = $_POST['email'];
+   $tlevel = $_POST['tlevel'];
 
-require 'dbconfig.php';
-if ($adminID == null || $email == null || $fname == '' || $lname == '' )
-{
-    header("refresh:2; url=addAdmin.php");
-    echo "Admin ID, First Name, Last Name or email was not supplied";
-}
-else
-{
-   
-     $sql = "Insert into adminmember(adminID, FirstName, LastName, Email, TechLevel) VALUES('".$hashPassword."','".$fname."','".$lname."','".$email."','".$tlevel."')";
-        if($con->query($sql) === TRUE)
-     {
-        header("refresh:2; url=addAdmin.php");
-         echo "Added admin user";
-     }
-     else
-     {
-        header("refresh:2; url=addAdmin.php");
-         echo "Error: " . $sql . "<br>" . $con->error;
-     }
- }
-     $con->close();
+   require 'dbconfig.php';
+   if ($adminID == null || $email == null || $fname == '' || $lname == '' )
+   {
+      header("refresh:2; url=addAdmin.php");
+      echo "Admin ID, First Name, Last Name or email was not supplied";
+   }
+   else
+   {
+      $sql1 = "Select * from adminmember where Email ='" . $email . "'";
+      $results = $con->query($sql1);
+
+      if($results->num_rows > 0)
+      {
+         //If email already exist user is not created"//
+         header("refresh:2; url=addAdmin.php");
+         echo "User Already Exist";
+      }
+      else
+      {
+         $sql = "Insert into adminmember(adminID, FirstName, LastName, Email, TechLevel) VALUES('".$hashPassword."','".$fname."','".$lname."','".$email."','".$tlevel."')";
+         if($con->query($sql) === TRUE)
+         {
+            header("refresh:2; url=addAdmin.php");
+            echo "Added admin user";
+         }
+         else
+         {
+            header("refresh:2; url=addAdmin.php");
+            echo "Error: " . $sql . "<br>" . $con->error;
+         }
+      }
+      $con->close();
+   }
 ?>
